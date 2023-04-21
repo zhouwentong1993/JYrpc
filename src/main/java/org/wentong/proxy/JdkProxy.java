6eea5d5576e4449f4044771294b68829f03d000a
zhouwentong <zhouwentong@hualala.com>
2023-04-21 14:33:58 +0800

    考虑清楚了，创建 proxy 工厂，获取指定类的代理类，代理类执行的逻辑是远程调用服务端

--- src/main/java/org/wentong/proxy/JdkProxy.java
+++
@@ -1,4 +0,0 @@
-package org.wentong.proxy;
-
-public class JdkProxy {
-}
--- src/main/java/org/wentong/proxy/ProxyFactory.java
+++ src/main/java/org/wentong/proxy/ProxyFactory.java
@@ -1,12 +1,22 @@
 package org.wentong.proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * 获取代理对象工厂
  */
 public class ProxyFactory {
 
-    public static <T> T getProxy(Class<T> clazz) {
-        return null;
+    public static <T> T refer(Class<T> clazz) {
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+                return null;
+            }
+        });
     }
 
 }
